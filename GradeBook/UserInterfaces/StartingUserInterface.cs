638b082076e042063d1feea65610aa2ddbf8bf07
Fernando Levi Marquardt <live@stiksy.net>
2018-03-29 22:01:23 -0300

    1. Increase Parts Check to 3 2. Update Validation Message 3. Instantiate The Gradebook 4. Update HelpCommand Message

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -40,7 +40,6 @@ namespace GradeBook.UserInterfaces
             }
             var name = parts[1];
             var type = parts[2].ToLower();
-            var weighted = parts[3];
             BaseGradeBook gradeBook;
             if (type == "standard")
                 gradeBook = new StandardGradeBook(name);
